ee4007d1a6df7d5d88120a40bc4b0e0b2008cf41
bob <namghjk@namghjks-MacBook-Pro.local>
2022-06-22 18:46:33 +0700

    Demo

---
+++ src/components/Login/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Row, Col, Button, Typography } from "antd";
+
+const { Title } = Typography;
+
+export default function Login() {
+  return (
+    <div>
+      <Row justify="center" style={{ height: 800 }}>
+        <Col span={8}>
+          <Title style={{ textAlign: "center" }} level={3}>
+            Fun chat
+          </Title>
+          <Button style={{ width: "100%", marginBottom: 5 }}>
+            Login with Google
+          </Button>
+          <Button style={{ width: "100%" }}>Login with Facebook</Button>
+        </Col>
+      </Row>
+    </div>
+  );
+}
